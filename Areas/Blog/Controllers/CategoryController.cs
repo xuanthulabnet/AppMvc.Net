582183353091fa94f2f369d08c323145244e4cec
XTLAB <xuanthulab.net@gmail.com>
2021-09-17 18:41:52 +0000

    Fix Category Parent

--- Areas/Blog/Controllers/CategoryController.cs
+++ Areas/Blog/Controllers/CategoryController.cs
@@ -165,6 +165,7 @@ namespace AppMvc.Net.Areas.Blog.Controllers
             return View(category);
         }
 
+
         // POST: Blog/Category/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -177,17 +178,59 @@ namespace AppMvc.Net.Areas.Blog.Controllers
                 return NotFound();
             }
 
+            bool canUpdate = true;
+
             if (category.ParentCategoryId  == category.Id)
             {
                 ModelState.AddModelError(string.Empty, "Phải chọn danh mục cha khác");
+                canUpdate = false;
             }
 
-            if (ModelState.IsValid && category.ParentCategoryId  != category.Id)
+            // Kiem tra thiet lap muc cha phu hop
+            if (canUpdate && category.ParentCategoryId != null)
+            { 
+            var childCates =  
+                        (from c in _context.Categories select c)
+                        .Include(c => c.CategoryChildren)
+                        .ToList()
+                        .Where(c => c.ParentCategoryId == category.Id);
+
+
+                // Func check Id 
+                Func<List<Category>, bool> checkCateIds = null;
+                checkCateIds = (cates) => 
+                    {
+                        foreach (var cate in cates)
+                        { 
+                             Console.WriteLine(cate.Title); 
+                            if (cate.Id == category.ParentCategoryId)
+                            {
+                                canUpdate = false;
+                                ModelState.AddModelError(string.Empty, "Phải chọn danh mục cha khácXX");
+                                return true;
+                            }
+                            if (cate.CategoryChildren!=null)
+                                return checkCateIds(cate.CategoryChildren.ToList());
+                          
+                        }
+                        return false;
+                    };
+                // End Func 
+                checkCateIds(childCates.ToList()); 
+            }
+
+
+
+
+            if (ModelState.IsValid && canUpdate)
             {
                 try
                 {
                     if (category.ParentCategoryId ==  -1)
                         category.ParentCategoryId = null;
+
+                    var dtc = _context.Categories.FirstOrDefault(c => c.Id == id);
+                    _context.Entry(dtc).State = EntityState.Detached;
 
                     _context.Update(category);
                     await _context.SaveChangesAsync();

48ddeece4d8197886ac8f99c665eea2bbd4db57f
XuanThuLab <xuanthulab.net@gmail.com>
2021-09-24 16:30:13 +0700

    Add Summernote

---
+++ Models/Summenote.cs
@@ -0,0 +1,30 @@
+namespace App.Models
+{
+    public class Summernote 
+    {
+        public Summernote(string iDEditor, bool loadLibrary  = true)
+        {
+            IDEditor = iDEditor;
+            LoadLibrary = loadLibrary;
+        }
+
+        public string IDEditor { get; set; }
+
+        public bool LoadLibrary { get; set; }
+
+        public int height { get; set; } = 120;
+
+        public string toolbar { get; set; } = @"
+            [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['height', ['height']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]       
+        ";
+    }
+}
